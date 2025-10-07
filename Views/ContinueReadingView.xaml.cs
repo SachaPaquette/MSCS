23e34fe24ed162b736e5e021fec0adb52ca735c4
Sacha <le1829079@crc-lennox.qc.ca>
2025-10-06 20:15:20 -0400

    Fixed opening the continue reading chapter

--- Views/ContinueReadingView.xaml.cs
+++ Views/ContinueReadingView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
+using MSCS.ViewModels;
 
 namespace MSCS.Views
 {
@@ -7,6 +9,19 @@ namespace MSCS.Views
         public ContinueReadingView()
         {
             InitializeComponent();
+        }
+
+        private void OnEntryClicked(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is System.Windows.Controls.ListViewItem item && item.DataContext is ContinueReadingEntryViewModel entry)
+            {
+                var command = entry.ContinueCommand;
+                if (command?.CanExecute(null) == true)
+                {
+                    command.Execute(null);
+                    e.Handled = true;
+                }
+            }
         }
     }
 }
