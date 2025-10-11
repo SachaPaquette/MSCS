aecacda2e9b88092f18bd6f1f1506d15b0a037dc
Sacha <le1829079@crc-lennox.qc.ca>
2025-10-11 15:55:42 -0400

    reworking settins page to be more modularized

---
+++ Views/Settings/SettingsSection.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MSCS.Views.Settings;
+
+public partial class SettingsSection : System.Windows.Controls.UserControl
+{
+    public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+        nameof(Title), typeof(string), typeof(SettingsSection), new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+        nameof(Description), typeof(string), typeof(SettingsSection), new PropertyMetadata(null));
+
+    public SettingsSection()
+    {
+        InitializeComponent();
+    }
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public string? Description
+    {
+        get => (string?)GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+}
