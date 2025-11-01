45a8ad4030e8eef6afde482a0f5c675c7c2a80ad
Sacha <le1829079@crc-lennox.qc.ca>
2025-11-01 19:23:10 -0400

    modularizing reader view

---
+++ Views/Reader/Controls/ReaderSeriesPanel.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace MSCS.Views.Reader
+{
+    public partial class ReaderSeriesPanel : UserControl
+    {
+        public ReaderSeriesPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
